b6dc0a1466d6e28ff475abc93f6ba4b71a87919a
ShadowSamurai29 <72855332+ShadowSamurai29@users.noreply.github.com>
2022-05-07 10:51:04 +0530

    Add files via upload

---
+++ MadLibs.py
@@ -0,0 +1,16 @@
+name = input("Enter Your Favourite Name: ")
+print("You have a nice name "+ name)
+
+color = input("Enter Your Favourite Color: ")
+print( color +" is one of my favourite color")
+
+celebrity = input("Enter Your Favourite Celebrity: ")
+print("Even I like " + celebrity)
+
+food = input("Enter Your Favourite Food: ")
+print("Even I like to eat " + food)
+
+song = input("Enter Your Favourite Song: ")
+print("The song " + song +" is bussing")
+
+
